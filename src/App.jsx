1b29ae1f1bb7d6dd993de7a7fd01be846fe4583a
ervin-diaz <Geineryodan@gmail.com>
2024-08-13 13:27:40 -0600

    Cart Page design finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/shop' element={<Shop />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
       </Routes>
     </BrowserRouter>
   );
