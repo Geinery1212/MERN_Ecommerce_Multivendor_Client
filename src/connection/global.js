be70f2996feee507cfbb82793e8fe103e7a02859
ervin-diaz <Geineryodan@gmail.com>
2024-11-08 16:54:21 -0600

    Public key stripe updaded

--- src/connection/global.js
+++ src/connection/global.js
@@ -1,4 +1,4 @@
 export const socket_connection = 'http://localhost:5000';
-export const PUBLIC_KEY_STRIPE = 'pk_test_51QG6oyJdfnu4zRGQtXJhKjjNZ1oVL7elujmxqLYVB7nDPAr2PcIb7LjsV73GlhhxefFVOEnouASGql38mryTl3MS00WkqTws4T';
+export const PUBLIC_KEY_STRIPE = 'pk_test_51QIxrkG6Xsy4FahsD5G9o4BD1JYiOKmzIotM0RaOtI9fdiSCkVLCwfebcvW99en2poI01Kxm6uUy9ZROp3i00Ff800EjogrfHQ';
 export const BASE_URL_API = 'http://localhost:5000/api';
 export const BASE_URL_CUSTOMER_SIDE = 'http://localhost:3001';
